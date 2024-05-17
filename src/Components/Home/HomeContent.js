92f45a43e234b35a2b452c0c1adf34c7e8427d7f
Sarankumar S <sarankumar152001@gmail.com>
2024-05-17 12:54:46 +0530

    latest code

--- src/Components/Home/HomeContent.js
+++ src/Components/Home/HomeContent.js
@@ -117,7 +117,7 @@ const HomeContent = () => {
                     printer took a galley of type and scrambled it to make a
                     type specimen book. It has survived not only five centuries,
                     but also the leap into electronic typesetting, remaining
-                    essentially unchanged.{" "}
+                    essentially unchanged to Electronics.{" "}
                   </p>
                 </div>
               </div>
